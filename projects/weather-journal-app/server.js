1db3a8b0bdb8a87d3346140eb5c0bb0380f77f36
Anna <anna2alt@gmail.com>
2021-03-03 21:59:59 +0300

    working app

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,43 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`Server is running on localhost: ${port}`);
+}
+
+app.get('/all', getAll);
+
+function getAll(req, res) {
+    res.send(projectData);
+}
+
+app.post('/addData', addData);
+
+function addData(req, res) {
+    const newData = req.body;
+    projectData.date = newData.date;
+    projectData.temp = newData.temp;
+    projectData.content = newData.content;
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,63 @@
 /* Global Variables */
+const baseZipUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '3d70271305a4b9c6c98c7b1fbbb5ecbe';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const zipCode = document.getElementById('zip').value;
+    getWeather(baseZipUrl, zipCode, apiKey)
+    .then( 
+        function(data) {
+            postData('/addData', {date: newDate, temp: data.main.temp, content: 'abc'});
+            updateUI();
+        }
+    )
+}
+
+const getWeather = async (baseUrl, zipCode, key)=>{
+ //   const res = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=3d70271305a4b9c6c98c7b1fbbb5ecbe');
+    const url = `${baseUrl}${zipCode}&appid=${key}`;
+    console.log(url);
+    const res = await fetch(url);
+    try {
+        // Transform into JSON
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+const postData = async (url, data)=>{
+    const response = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });   
+}
+
+const updateUI = async ()=>{
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        console.log('allData:');
+        console.log(allData);
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = allData.content;
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+}
